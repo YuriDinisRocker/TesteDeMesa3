53e1e43975d5213869eca6147825ac35b67ada2a
Yuri Rocker - E. Escolinha de TI - BNE <yurirocker@bne.com.br>
2025-10-20 17:31:39 -0300

    Teste de Mesa Commit

---
+++ Calculo.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesteDeMesa3
+{
+    internal class Calculo
+    {
+        private float valorInicial;
+        private float valorFinal;
+        private float periodo;
+        private List<float> rendimentosMensais = new List<float>();
+        private float taxa;
+        private List<float> valoresRendimentos = new List<float>();
+
+        public Calculo()
+        {
+
+        }
+
+        public List<float> calcularJurosSemResgate(float valorInicial, float taxa, float periodo)
+        {
+            this.valorInicial = valorInicial; this.taxa = taxa; this.periodo = periodo; float periodoCount = 0;
+            this.valoresRendimentos.Add(this.valorInicial);
+            for (int i=1; i<=(int)periodo; i++)
+            {
+                if(this.periodo - (float)i == 0)
+                {
+                    periodoCount = this.periodo;
+                }
+                else
+                {
+                    periodoCount = this.periodo - (this.periodo - (float)i);
+                }
+                    float resultado = this.valorInicial * (float)Math.Pow((1 + this.taxa), periodoCount);
+                this.valorFinal = resultado;
+                this.valoresRendimentos.Add(this.valorFinal);
+            }
+            return valoresRendimentos;
+        }
+        
+        ////////////////////////////////////////////////////
+
+        public List<float> calcularJurosComResgate(float valorInicial, float taxa, float periodo, List<int> periodoResgate, float resgate)
+        {
+            this.valorInicial = valorInicial; this.taxa = taxa; this.periodo = periodo; float periodoCount = 0;
+            this.valoresRendimentos.Add(this.valorInicial);
+            float id = 0;
+            for (int i = 1; i <= (int)periodo; i++)
+            {
+                id++;
+
+                if (periodoResgate.Contains(i))
+                {
+                    id = 1;
+                    this.valorInicial -= resgate;
+                }
+
+                if (this.periodo - (float)id == 0)
+                {
+                    periodoCount = this.periodo;
+                }
+                else
+                {
+                    periodoCount = this.periodo - (this.periodo - (float)id);
+                }
+
+                float resultado = this.valorInicial * (float)Math.Pow((1 + this.taxa), periodoCount);
+                this.valorFinal = resultado;
+                this.valoresRendimentos.Add(this.valorFinal);
+            }
+            return valoresRendimentos;
+        }
+
+        //////////////////////////////////////////////////////////
+
+        public List<float> calcularRendimentoMensal(float valorInicial, float valorFinal)
+        {
+            for(int i=0; i < (this.valoresRendimentos.Count() -1 ); i++)
+            {
+                float rendimentoMensal = valoresRendimentos[i + 1] - valoresRendimentos[i];
+                rendimentosMensais.Add(rendimentoMensal);
+            }
+            return rendimentosMensais;
+
+        }
+
+        public float converterData(float ano, float mes, float dia)
+        {
+            float calculoAno = ano * 12;
+            float calculoDia = dia / 30;
+            float res = calculoAno + calculoDia + mes;
+            return res;
+        }
+
+        public float rendimentoBrutoTotal()
+        {
+            return valoresRendimentos.Last();
+        }
+
+        public float rendimentoLiquidoTotal()
+        {
+            return rendimentosMensais.Sum();
+        }
+
+
+        public void limparListas()
+        {
+            rendimentosMensais.Clear();
+            valoresRendimentos.Clear();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,145 @@
+﻿using TesteDeMesa3;
+
+public class Program
+{
+
+    public static void Main(string[] args)
+    {
+        String escolha; int questao; float valorInicial; float taxa; float periodoAno; float periodoMes; float periodoDia; float periodoTotal;
+        float valorResgateMensal=0; String escolhaFinal = ""; int mes; List<float> rendimentos = new List<float>();
+        List<int> resgateMensal = new List<int>();
+        List<float> valorFinal;
+        Calculo calc=new Calculo();
+        Console.WriteLine("TESTE DE MESA 3");
+
+        Console.Write("1 - atividade 1 | 2 - atividade 2: ");
+        escolha = Console.ReadLine();
+        int.TryParse(escolha, out questao);
+        switch (questao)
+        {
+            case 1:
+                Console.Write("QUESTÃO 1:\nInforme o valor inicial do investimento:");
+                escolha=Console.ReadLine();
+                float.TryParse(escolha, out valorInicial);
+                Console.Write("\nInforme a quantidade de anos que o investimento se manterá:");
+                escolha = Console.ReadLine();
+                float.TryParse(escolha, out periodoAno);
+                Console.Write("\nInforme a quantidade de meses que o investimento se manterá:");
+                escolha = Console.ReadLine();
+                float.TryParse(escolha, out periodoMes);
+                Console.Write("\nInforme a quantidade de dias que o investimento se manterá:");
+                escolha = Console.ReadLine();
+                float.TryParse(escolha, out periodoDia);
+                Console.Write("\nInforme a taxa de juros: ");
+                escolha = Console.ReadLine();
+                float.TryParse(escolha, out taxa);
+                Console.WriteLine("\nA taxa está em formato: A - anual | M - mensal | D - diario ");
+                escolha = Console.ReadLine();
+                if(escolha == "A")
+                {
+                    taxa /= 12;
+                    taxa /= 100;
+                }else if(escolha == "M")
+                {
+                    taxa /= 100;
+                }
+                else
+                {
+                    taxa *=30;
+                    taxa /= 100;
+                }
+                periodoTotal=calc.converterData(periodoAno, periodoMes, periodoDia);
+                calc.calcularJurosSemResgate(valorInicial, taxa, periodoTotal);
+                calc.calcularRendimentoMensal(valorInicial, calc.rendimentoBrutoTotal());
+                Console.Clear();
+                Console.WriteLine($"Valor Inicial: R${valorInicial}");
+                Console.WriteLine($"Porcentagem da taxa: %{taxa * 100}");
+                Console.WriteLine($"Periodo e investimento de {periodoAno} - Anos | {periodoMes} - Meses | {periodoDia} - Dias");
+                Console.WriteLine($"Rendimento bruto: R${calc.rendimentoBrutoTotal():F2}");
+                Console.WriteLine($"Rendimento líquido total: R${calc.rendimentoLiquidoTotal():F2}");
+                calc.limparListas();
+                break;
+
+            case 2:
+                Console.Write("QUESTÃO 2:\nInforme o valor inicial do investimento:");
+                escolha = Console.ReadLine();
+                float.TryParse(escolha, out valorInicial);
+                Console.Write("\nInforme a quantidade de anos que o investimento se manterá:");
+                escolha = Console.ReadLine();
+                float.TryParse(escolha, out periodoAno);
+                Console.Write("\nInforme a quantidade de meses que o investimento se manterá:");
+                escolha = Console.ReadLine();
+                float.TryParse(escolha, out periodoMes);
+                Console.Write("\nInforme a quantidade de dias que o investimento se manterá:");
+                escolha = Console.ReadLine();
+                float.TryParse(escolha, out periodoDia);
+                Console.Write("\nInforme a taxa de juros: ");
+                escolha = Console.ReadLine();
+                float.TryParse(escolha, out taxa);
+                Console.WriteLine("\nA taxa está em formato: A - anual | M - mensal | D - diario ");
+                escolha = Console.ReadLine();
+                if (escolha == "A")
+                {
+                    taxa /= 12;
+                    taxa /= 100;
+                }
+                else if (escolha == "M")
+                {
+                    taxa /= 100;
+                }
+                else
+                {
+                    taxa *= 30;
+                    taxa /= 100;
+                }
+                periodoTotal = calc.converterData(periodoAno, periodoMes, periodoDia);
+                Console.Write("Deseja resgatar alguma quantia antes da finalização: S - sim | N - não");
+                escolha = Console.ReadLine();
+                if(escolha == "S")
+                {
+                    Console.WriteLine("Informe o valor de resgate: ");
+                    escolha = Console.ReadLine();
+                    float.TryParse(escolha, out valorResgateMensal);
+                    Console.WriteLine("Informe os meses que deseja resgatar um valor: ");
+                    while (escolhaFinal != "N")
+                    {
+
+                        Console.Write($"Digite um número entre {periodoTotal:F2}: ");
+                        escolha = Console.ReadLine();
+                        int.TryParse(escolha, out mes);
+                        resgateMensal.Add(mes);
+                        Console.Write("\nDeseja resgatar algum valor em mais um mês: S - sim | N - não");
+                        escolhaFinal=Console.ReadLine();
+                    }
+                }
+                
+                rendimentos=calc.calcularJurosComResgate(valorInicial, taxa, periodoTotal, resgateMensal, valorResgateMensal);
+                calc.calcularRendimentoMensal(valorInicial, calc.rendimentoBrutoTotal());
+                Console.Clear();
+                for(int i=0; i<periodoTotal; i++)
+                {
+                    Console.Write("\n\n\n");
+                    Console.WriteLine($"-------------------------ESTATISTICAS MÊS {i+1}-------------------------");
+                    if (resgateMensal.Contains(i))
+                    {
+                        Console.WriteLine($"Valor Inicial: R${(valorInicial - valorResgateMensal):F2}");
+                        valorInicial -= valorResgateMensal;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Valor Inicial: R${valorInicial:F2}");
+                    }
+
+                    Console.WriteLine($"Porcentagem da taxa: %{taxa * 100}");
+                    Console.WriteLine($"Rendimento bruto: R${rendimentos[i]:F2}");
+                 
+                }
+                break;
+
+            default:
+                break;
+
+        }
+
+    }
+}
